ab623bd639286708256bd701f968e6d1260f7b72
Catalina Avila Rotela <avilarotelcatlina@gmail.com>
2024-04-21 14:19:44 -0300

    Creación de clases, duda con respecto a propiedades de la clase articulos

---
+++ WindowsFormsApp1/Articulo.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class Articulo
+    {
+        //ATRIBUTOS
+        private string CodArt;
+        private string Nombre;
+        private string Descripción;
+        private int Precio;
+        private Marca Marca;
+        private Categoria Categoria;
+        private Imagen Imagen;
+
+        //PROPIEDADES
+        public string Nombre
+        { 
+            get { return Nombre; }
+            set { Nombre = value; }
+        
+        }
+
+        public string Descripcion
+        {
+            get { return Descripcion; }
+            set { Descripcion = value; }
+
+        }
+
+        public int Precio
+        {
+            get { return Precio; }
+            set { Precio = value; }
+
+        }
+
+        public Marca Marca
+        {
+            get { return Marca; }
+            set { Marca = value; }
+
+        }
+
+        public Categoria Categoria
+        {
+            get { return Categoria; }
+            set { Categoria = value; }
+
+        }
+
+        public string Imagen
+        {
+            get { return Imagen; }
+            set { Imagen = value; }
+
+        }
+
+    }
+}
